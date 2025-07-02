3094cadeac391f63db3f4d851386615dc3aafbea
Arnoldus Bryan Hendry <bryanhendry12@gmail.com>
2025-07-03 01:18:08 +0700

    update ui with tailwind, with active nav link

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
+    <x-slot:heading>
+        About Page
+    </x-slot:heading>
     <h1>Hello this is the about page</h1>
 </x-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
+    <x-slot:heading>
+        Home Page
+    </x-slot:heading>
     <h1>hello this is my homepage!</h1>
 </x-layout>
